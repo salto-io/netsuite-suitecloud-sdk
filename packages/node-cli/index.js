84b9988cd5b14674cf2aba6ce71a979f14aa1474
Omri Litvak <omri.litvak@salto.io>
2020-03-26 10:04:00 +0200

    export relevant classes to trigger commands out of the package

---
+++ packages/node-cli/index.js
@@ -0,0 +1,7 @@
+module.exports.CommandsMetadataService = require('./src/core/CommandsMetadataService');
+module.exports.CommandActionExecutor = require('./src/core/CommandActionExecutor');
+module.exports.CommandInstanceFactory = require('./src/core/CommandInstanceFactory');
+module.exports.CommandOptionsValidator = require('./src/core/CommandOptionsValidator');
+module.exports.CommandOutputHandler = require('./src/core/CommandOutputHandler');
+module.exports.CLIConfigurationService = require('./src/core/extensibility/CLIConfigurationService');
+module.exports.AuthenticationService = require('./src/core/authentication/AuthenticationService');
