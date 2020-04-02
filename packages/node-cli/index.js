9a404e2b88823d07ca3b4ada778600f6ffc17883
Omri Litvak <omri.litvak@salto.io>
2020-04-02 12:36:42 +0300

    Expose top level index.js

--- packages/node-cli/index.js
+++ packages/node-cli/index.js
@@ -1,7 +1,9 @@
-module.exports.CommandsMetadataService = require('./src/core/CommandsMetadataService');
-module.exports.CommandActionExecutor = require('./src/core/CommandActionExecutor');
-module.exports.CommandInstanceFactory = require('./src/core/CommandInstanceFactory');
-module.exports.CommandOptionsValidator = require('./src/core/CommandOptionsValidator');
-module.exports.CommandOutputHandler = require('./src/core/CommandOutputHandler');
-module.exports.CLIConfigurationService = require('./src/core/extensibility/CLIConfigurationService');
-module.exports.AuthenticationService = require('./src/core/authentication/AuthenticationService');
+module.exports = {
+	CommandsMetadataService: require('./src/core/CommandsMetadataService'),
+	CommandActionExecutor: require('./src/core/CommandActionExecutor'),
+	CommandInstanceFactory: require('./src/core/CommandInstanceFactory'),
+	CommandOptionsValidator: require('./src/core/CommandOptionsValidator'),
+	CommandOutputHandler: require('./src/core/CommandOutputHandler'),
+	CLIConfigurationService: require('./src/core/extensibility/CLIConfigurationService'),
+	AuthenticationService: require('./src/core/authentication/AuthenticationService'),
+};
