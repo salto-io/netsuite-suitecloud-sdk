e7b2ca692b1c4dcdcb41db6390e0876e10cf6998
roironn <roi.ronn@salto.io>
2020-09-10 16:38:24 +0300

    Export sdk download and make sure downloads only once, remove postinstall script, return download result
    (cherry picked from commit be6ab8116c5c71c29749ad0917eb73d699de7762)
    (cherry picked from commit 6316dce3d517702a4f17c2050acbd3f90a616a58)
    (cherry picked from commit 3a8787a152b4e831a7f211986f38b37224db9ef8)
    (cherry picked from commit 8897d1b9a0dfe42eeda256d1693c2dc4756751b2)
    (cherry picked from commit 2f003653c593756261c9c3a168630f1c92b569ad)
    
    (cherry picked from commit 9d61d19129c72267aafef16c36959fb79efcea8f)
    
    (cherry picked from commit bf0d67d85a2b42b78d34f9e6466ca92603b4da91)
    
    (cherry picked from commit e1a8d75a75f3165e87eea7343237e6b9e84ab5ee)

--- packages/node-cli/src/ApplicationConstants.js
+++ packages/node-cli/src/ApplicationConstants.js
@@ -4,6 +4,8 @@
  */
 'use strict';
 
+const SDK_VERSION = '2022.2.1';
+
 module.exports = {
 	AUTHORIZATION_PROPERTIES_KEYS: {
 		NEEDS_REAUTHORIZATION: 'needsReauthorization',
@@ -13,6 +15,9 @@ module.exports = {
 	SDK_COMMANDS_METADATA_PATCH_FILE: 'metadata/SdkCommandsMetadataPatch.json', //Overwrites selected properties in SdkCommandsMetadata.json file that are specific for SuiteCloud CLI for Node.js
 	NODE_COMMANDS_METADATA_FILE: 'metadata/NodeCommandsMetadata.json',
 	COMMAND_GENERATORS_METADATA_FILE: 'metadata/CommandGenerators.json',
+	SDK_VERSION,
+	COMPATIBLE_NS_VERSION: '2022.2',
+	SDK_FILENAME: `cli-${SDK_VERSION}.jar`,
 	SDK_COMPATIBLE_JAVA_VERSIONS: ['17', '21'],
 	SDK_INTEGRATION_MODE_JVM_OPTION: '-DintegrationMode --add-opens java.base/java.io=ALL-UNNAMED',
 	SDK_CLIENT_PLATFORM: 'SuiteCloudCLIforNode.js',
