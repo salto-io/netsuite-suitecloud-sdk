e677e4236bd37370ce9bb05a2c65733dfc5dea27
Omri Litvak <9881966+omrilit@users.noreply.github.com>
2024-10-20 10:18:18 +0300

    SAAS-2781: Remove Spinner prints in the SaaS so the logs will be indexed (#37)

--- packages/node-cli/src/metadata/NodeCommandsMetadata.js
+++ packages/node-cli/src/metadata/NodeCommandsMetadata.js
@@ -1,46 +1,4 @@
 module.exports = {
-	"account:savetoken": {
-		"name": "account:savetoken",
-		"description": "Saves a TBA token that you issued previously in NetSuite.",
-        "isSetupRequired": false,
-        "options": {
-            "account": {
-                "name": "account",
-                "description": "References the ID of the account to log in to.",
-                "mandatory": true,
-				"type": "SINGLE",
-				"disableInIntegrationMode": false
-            },
-            "authid": {
-                "name": "authid",
-                "description": "References the custom name you gave to a specific account-role combination.",
-                "mandatory": true,
-				"type": "SINGLE",
-				"disableInIntegrationMode": false
-            },
-            "tokenid": {
-                "name": "tokenid",
-                "description": "Enter the token ID of a TBA token you issued previously in NetSuite.",
-                "mandatory": true,
-				"type": "SINGLE",
-				"disableInIntegrationMode": false
-            },
-            "tokensecret": {
-                "name": "tokensecret",
-                "description": "Enter the token secret of a TBA token you issued previously in NetSuite.",
-                "mandatory": true,
-				"type": "SINGLE",
-				"disableInIntegrationMode": false
-            },
-            "url": {
-                "name": "url",
-                "description": "Specify the NetSuite domain of the account you want to use. You only need to specify the NetSuite domain if you are not using a production account.",
-                "mandatory": false,
-				"type": "SINGLE",
-				"disableInIntegrationMode": false
-            }
-        }
-    },
     "account:setup": {
         "name": "account:setup",
         "alias": "sa",
